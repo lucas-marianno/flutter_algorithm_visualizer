0a7d5ae8d78961358541641ac0a1d8d80bf9773d
Lucas Moraes <lucas.pk16@outlook.com>
2024-04-26 17:57:05 -0300

    bars slider + speed slider (WIP)

--- lib/main.dart
+++ lib/main.dart
@@ -25,17 +25,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final int nOfBars = 10;
-  final Duration animationInterval = const Duration(milliseconds: 1);
+  int nOfBars = 100;
+  int sortingSpeed = 2;
+  final Duration animationInterval = const Duration(microseconds: 1);
   final int barHeight = 400;
 
   bool stopSort = false;
   List<Bar> bars = [];
-  int multiplier = 1;
+  double multiplier = 1;
 
   void populate(int quantity) {
+    bars.clear();
+
+    multiplier = barHeight / nOfBars;
+
     for (int i = 1; i <= quantity; i++) {
-      bars.add(Bar(i * multiplier));
+      bars.add(Bar((i * multiplier).toInt()));
     }
   }
 
@@ -49,14 +54,15 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  void bubbleSort(int steps) async {
-    if (steps <= 1 || stopSort) return;
-
+  Future<void> bubbleSort(int steps, {bool isSorted = false}) async {
+    if (steps <= 1 || stopSort || isSorted) return;
+    isSorted = true;
     for (int i = 0; i < steps - 1; i++) {
       bars[i] = Bar(bars[i].value, selected: true);
       bars[i + 1] = Bar(bars[i + 1].value, selected: true);
       setState(() {});
       if (bars[i].value > bars[i + 1].value) {
+        isSorted = false;
         final Bar tempBar = bars[i];
         bars[i] = bars[i + 1];
         bars[i + 1] = tempBar;
@@ -65,14 +71,26 @@ class _HomePageState extends State<HomePage> {
       await Future.delayed(animationInterval);
       bars[i] = Bar(bars[i].value);
       bars[i + 1] = Bar(bars[i + 1].value);
-      setState(() {});
     }
-    bubbleSort(steps - 1);
+    setState(() {});
+    bubbleSort(steps - 1, isSorted: isSorted);
+  }
+
+  String sortingSpeedLabel() {
+    switch (sortingSpeed) {
+      case 1:
+        return 'slow af';
+      case 2:
+        return 'slow';
+      case 3:
+        return 'fast';
+      default:
+        return 'instant';
+    }
   }
 
   @override
   void initState() {
-    multiplier = barHeight ~/ nOfBars;
     populate(nOfBars);
     super.initState();
   }
@@ -96,6 +114,35 @@ class _HomePageState extends State<HomePage> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
+                  Row(
+                    children: [
+                      Slider(
+                        label: nOfBars.toString(),
+                        value: nOfBars.toDouble(),
+                        divisions: 100,
+                        min: 2,
+                        max: 100,
+                        onChanged: (value) {
+                          stopSort = true;
+                          setState(() {});
+                          nOfBars = value.toInt();
+                          populate(nOfBars);
+                          setState(() {});
+                        },
+                      ),
+                      Slider(
+                        value: sortingSpeed.toDouble(),
+                        label: sortingSpeedLabel(),
+                        min: 1,
+                        max: 4,
+                        divisions: 3,
+                        onChanged: (speed) {
+                          sortingSpeed = speed.toInt();
+                          setState(() {});
+                        },
+                      )
+                    ],
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [

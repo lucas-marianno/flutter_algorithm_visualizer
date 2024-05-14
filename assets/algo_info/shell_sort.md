52ddb62ab21731920b23d222d1a4054ec08a64e7
Lucas Marianno <lucas.marianno94@gmail.com>
2024-05-13 23:00:39 -0300

    shell sort implemented + animations

---
+++ assets/algo_info/shell_sort.md
@@ -0,0 +1,31 @@
+#	Shell sort
+
+### Space-time complexity:
+- O(n²) - O(n log n)
+
+### Brief algorithm description:
+- Start with a gap value, typically half the length of the list, and reduce this gap size in each iteration. Compare elements that are 'gap' distance apart and swap them if necessary to put them in the correct order. Continue this process with decreasing gap sizes until the gap becomes 1, at which point the algorithm behaves like an insertion sort. Finally, perform a normal insertion sort on the entire list.
+- Improved insertion sort
+
+### Implementation in Dart:
+
+```
+
+void shellSort(List arr) {
+  int n = arr.length;
+  
+  for (int gap = n ~/ 2; gap > 0; gap ~/= 2) {
+    for (int i = gap; i < n; i += 1) {
+      int temp = arr[i];
+  
+      int j;
+      for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+        arr[j] = arr[j - gap];
+      }
+  
+      arr[j] = temp;
+    }
+  }
+}
+
+```
---
+++ lib/logic/sorting_algorithms/shell_sort.dart
@@ -0,0 +1,48 @@
+import 'package:algorithm_visualizer/logic/sorting_controller.dart';
+import 'package:algorithm_visualizer/widgets/bar.dart';
+import 'package:flutter/material.dart';
+
+Future<void> shell(
+  List<Bar> bars,
+  Future<void> Function(List<Bar> newBar) updateBarsGraph,
+  void Function() registerOperation,
+) async {
+  Future<void> colorize(List<int> index, {Color? color = Colors.amber}) async {
+    for (int i in index) {
+      bars[i] = Bar(bars[i].value, color: color);
+    }
+    await updateBarsGraph(bars);
+    for (int i in index) {
+      bars[i] = Bar(bars[i].value);
+    }
+    updateBarsGraph(bars);
+  }
+
+  Future<List<Bar>> shellSort(List<Bar> barsList) async {
+    int n = barsList.length;
+
+    for (int gap = n ~/ 2; gap > 0; gap ~/= 2) {
+      for (int i = gap; i < n; i++) {
+        if (SortingControllerState().hasStopped) return barsList;
+        await colorize([i]);
+        registerOperation();
+
+        Bar temp = barsList[i];
+
+        int j;
+        for (j = i; j >= gap && barsList[j - gap].value > temp.value; j -= gap) {
+          if (SortingControllerState().hasStopped) return barsList;
+          await colorize([i, j - gap], color: Colors.red);
+          registerOperation();
+
+          barsList[j] = barsList[j - gap];
+        }
+
+        barsList[j] = temp;
+      }
+    }
+    return barsList;
+  }
+
+  await updateBarsGraph(await shellSort(bars));
+}
